32ee1a6b6855658b7f504817458332319b4b8684
Gustavo <gustavo.prog40@gmail.com>
2021-12-14 20:48:27 -0300

    feito

--- styles/styled-index.js
+++ styles/styled-index.js
@@ -4,18 +4,18 @@ import Image from 'next/image'
 export const ContainerImage = styled.div`
   margin-left: 293px;
   margin-top: -492px;
-  transition: ease 0.9s;
+  transition: 0.2s;
   transform: scale(1);
   width: 65px;
   border-radius: 100%;
 
   :hover {
-    width: 80px;
+    width: 70px;
     transform: scale(1);
   }
 `
 export const Imagem = styled.image`
-  transition: ease 1.8s;
+  transition: ease 0.1s;
   transform: scale(1);
   background: green;
 
