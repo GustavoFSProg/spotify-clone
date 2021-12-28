3dc497f2b133993379d771f129caf2f0ee5110c4
Gustavo <gustavo.prog40@gmail.com>
2021-12-28 13:40:29 -0300

    header

--- src/components/Main/Header/index.js
+++ src/components/Main/Header/index.js
@@ -1,5 +1,13 @@
 import { SectionHeader } from './styles'
+import image from '/public/images/album-image.jpg'
+import Image from 'next/image'
 
-const Header = () => <SectionHeader>Implement header section</SectionHeader>
+const Header = () => (
+  <SectionHeader>
+    <div style={{ marginTop: '60px', marginLeft: '15px' }}>
+      <Image src={image} alt="imagem" width="100" height="230" />
+    </div>
+  </SectionHeader>
+)
 
 export default Header
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,36 @@
 import Header from './Header'
+import { SectionHeader } from './Header/styles'
 import MainPlayer from './MainPlayer'
 import PlayList from './PlayList'
 import { MainContainer } from './styles'
+import Image from 'next/image'
+import image from '/public/images/album-image.jpg'
 
 const Main = () => (
   <MainContainer>
-    <Header />
+    <SectionHeader>
+      <div style={{ marginTop: '60px', marginLeft: '15px' }}>
+        <Image src={image} alt="imagem" width="190" height="210" />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            marginLeft: '13.3rem',
+            marginTop: '-100px',
+          }}
+        >
+          <span style={{ fontSize: '11px', fontWeight: 'bold' }}>LISTA DE REPRODUÇÃO</span>
+          <span style={{ fontSize: '46px', fontWeight: 'bold', marginTop: '15px' }}>
+            Blood Magic Necromance
+          </span>
+          <span style={{ fontSize: '14px', fontWeight: 'bold', marginTop: '22px' }}>
+            Gustavo Sohne .{' '}
+            <strong style={{ color: 'grey', fontWeight: 'normal' }}>8 músicas, 45 min 44s</strong>
+          </span>
+        </div>
+      </div>
+    </SectionHeader>
     <MainPlayer />
     <PlayList />
   </MainContainer>
