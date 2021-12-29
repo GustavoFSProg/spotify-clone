fc4b240fee8cb9b64f5f23ed129560f58316709b
Gustavo <gustavo.prog40@gmail.com>
2021-12-29 16:56:13 -0300

    footer

--- src/components/Footer/Player/index.js
+++ src/components/Footer/Player/index.js
@@ -7,23 +7,31 @@ import Play from '../../../../public/images/button-play.png'
 const Player = () => (
   <PlayerContainer>
     <Image src={Play} alt="play-button" width="40" height="30" />
-    <div
-      style={{
-        background: '#595959',
-        borderRadius: '20px',
-        width: '420px',
-        marginTop: '15px',
-        height: '4px',
-      }}
-    >
+    <div style={{ display: 'flex', flexDirection: 'row', marginTop: '6px' }}>
+      <span style={{ color: '#bfbfbf', fontSize: '13.2px', marginTop: '6px', marginRight: '7px' }}>
+        1:35
+      </span>
       <div
         style={{
-          background: '#bfbfbf',
+          background: '#595959',
           borderRadius: '20px',
-          width: '100px',
+          width: '420px',
+          marginTop: '15px',
           height: '4px',
         }}
-      ></div>
+      >
+        <div
+          style={{
+            background: '#bfbfbf',
+            borderRadius: '20px',
+            width: '150px',
+            height: '4px',
+          }}
+        />
+      </div>
+      <span style={{ color: '#bfbfbf', fontSize: '13.2px', marginTop: '6px', marginLeft: '7px' }}>
+        8:35
+      </span>
     </div>
   </PlayerContainer>
 )
