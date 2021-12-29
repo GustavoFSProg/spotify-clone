ecd762fde4242ce491c0d180d39500c0c28e04fd
Gustavo <gustavo.prog40@gmail.com>
2021-12-29 09:41:51 -0300

    button-play

--- src/components/Footer/Player/index.js
+++ src/components/Footer/Player/index.js
@@ -1,7 +1,32 @@
-import { memo } from 'react'
+// import { memo } from 'react'
 
 import { PlayerContainer } from './styles'
+import Image from 'next/image'
+import Play from '../../../../public/images/play-button-display.png'
 
-const Player = () => <PlayerContainer>Implementar o Player</PlayerContainer>
+const Player = () => (
+  <PlayerContainer>
+    <Image src={Play} alt="play-button" width="30" height="30" />
+    <div
+      style={{
+        background: '#595959',
+        borderRadius: '20px',
+        width: '380px',
+        marginTop: '15px',
+        height: '5px',
+      }}
+    >
+      <div
+        style={{
+          background: '#bfbfbf',
+          borderRadius: '20px',
 
-export default memo(Player)
+          width: '100px',
+          height: '5px',
+        }}
+      ></div>
+    </div>
+  </PlayerContainer>
+)
+
+export default Player
--- src/components/Footer/Player/styles.js
+++ src/components/Footer/Player/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const PlayerContainer = styled.div`
   border: 1px dashed darkgray;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   max-width: 722px;
